0e40b11f427d7f88f50ee4ea76b5d94012922e97
Azhar Prabudi <azharprabui@gmail.com>
2019-05-22 00:16:57 +0700

    add id whenever sendlog created

--- lib/model/send_log.dart
+++ lib/model/send_log.dart
@@ -4,24 +4,28 @@ import 'package:fazz_tools/model/stringify.dart';
 import 'package:fazz_tools/value/log_display.dart';
 import 'package:fazz_tools/value/log_type.dart';
 import 'package:meta/meta.dart';
+import 'package:uuid/uuid.dart';
 
 class SendLog implements Stringify {
   dynamic text;
   String type;
   String showAs;
   PrintOptions options;
+  String _id;
 
   SendLog({
     @required this.text,
     String type = '',
     String showAs = '',
     PrintOptions options,
-  })  : this.type = type == '' ? LogType.text : type,
+  })  : this._id = Uuid().v4(),
+        this.type = type == '' ? LogType.text : type,
         this.showAs = showAs == '' ? LogDisplay.plainText : showAs,
         this.options = options == null ? PrintOptions() : options;
 
   @override
   Map<String, dynamic> toJson() => {
+        "_id": _id,
         "text": text,
         "type": type,
         "showAs": showAs,
