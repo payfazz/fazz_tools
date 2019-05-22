64f611601b88e1a10636d2281d024bdf1982ced4
Azhar Prabudi <azharprabui@gmail.com>
2019-05-22 12:42:08 +0700

    parse http response header to map

--- lib/tools/network_inspector/dio_inspector.dart
+++ lib/tools/network_inspector/dio_inspector.dart
@@ -1,6 +1,6 @@
-import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:fazz_tools/model/http_result.dart';
+import 'package:fazz_tools/model/print_options.dart';
 import 'package:fazz_tools/model/send_log.dart';
 import 'package:fazz_tools/value/log_type.dart';
 import 'package:meta/meta.dart';
@@ -21,6 +21,9 @@ class FazzDioInspector extends InterceptorsWrapper {
     ws.sink.add(
       SendLog(
         text: err.message,
+        options: PrintOptions(
+          color: "red",
+        ),
       ).stringify(),
     );
     _sendHTTPResult(err.response, isError: true);
@@ -28,17 +31,25 @@ class FazzDioInspector extends InterceptorsWrapper {
   }
 
   void _sendHTTPResult(Response response, {isError = false}) {
+    String baseUrl =
+        response.request.baseUrl == null ? "" : response.request.baseUrl;
+    String path = response.request.path == null ? "" : response.request.path;
+
+    Map<String, List<String>> responseHeaders = {};
+    response.headers.forEach((name, value) {
+      responseHeaders[name] = value;
+    });
+
     ws.sink.add(
       SendLog(
         text: HttpResult(
-          url: response.realUri.toString(),
+          url: '$baseUrl$path',
           method: response.request.method,
           body: response.request.data,
           params: response.request.queryParameters,
           requestHeaders: response.request.headers,
           responseBody: response.data,
-          responseHeaders:
-              jsonDecode(jsonEncode(response.headers)) as Map<String, dynamic>,
+          responseHeaders: responseHeaders,
           statusCode: response.statusCode,
           isError: isError,
         ).toJson(),
